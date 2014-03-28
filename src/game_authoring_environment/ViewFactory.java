78e551e5959caa2f8ff74c70b26c0e1ee9599e5b
KatKrieger <kat.krieger@gmail.com>
2014-03-28 14:14:38 -0400

    view factory

---
+++ src/game_authoring_environment/ViewFactory.java
@@ -0,0 +1,15 @@
+package game_authoring_environment;
+
+import javax.swing.JButton;
+
+public class ViewFactory {
+	
+	public ViewFactory(){
+		
+	}
+	
+	public JButton createJButton(String str){
+		JButton button = new JButton(str);
+		return button;
+	}
+}
