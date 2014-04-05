49dea08d2149f140901d1ad191ad5427ae90fc79
Justin (Zihao) Zhang <Main@wl-10-190-32-161.wireless.duke.edu>
2014-04-04 21:18:17 -0400

    create behaviors

---
+++ src/behaviors/Movable.java
@@ -0,0 +1,13 @@
+package behaviors;
+
+import objects.GameObject;
+
+public abstract class Movable {
+	protected GameObject myObject; 
+	
+	public Movable(GameObject o){
+		myObject = o;
+	}
+	
+	public abstract void move(double xspeed, double yspeed);
+}
--- src/objects/GameObject.java
+++ src/objects/GameObject.java
@@ -1,5 +1,34 @@
 package objects;
 
-public class GameObject {
+import java.util.ResourceBundle;
 
+import jboxGlue.PhysicalObject;
+import jgame.JGColor;
+
+public abstract class GameObject extends PhysicalObject{
+    public static final String DEFAULT_RESOURCE_PACKAGE = "engineResources/";
+    public static final String DEFAULT_BEHAVIOR = "";
+    
+	protected ResourceBundle myDieWays;
+	protected ResourceBundle myMoveWays;
+
+	protected GameObject(String name, int collisionId, JGColor color) {
+		super(name, collisionId, color);
+	}
+	
+	protected void initObject(){
+		myDieWays = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_BEHAVIOR);
+	}
+	
+	protected GameObject(String name, int collisionId, String gfxname){
+		super(name, collisionId, gfxname);
+	}
+	
+	public setDieBehavior(String s){
+		
+	}
+	
+	public void die(){
+		
+	}
 }
