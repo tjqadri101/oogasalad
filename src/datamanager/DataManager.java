41fc2083e059ad76825d529a1fe2c43780547399
Steve Wang <sw211@duke.edu>
2014-04-04 22:21:47 -0400

    DM initial commit

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -1,5 +1,33 @@
 package datamanager;
 
-public class DataManager {
+import java.util.*;
+import java.util.logging.Level;
+import jgame.JGObject;
 
+public class DataManager {
+    private String order = "m3 attribute1 attribute2 attribute3 l1 s3";
+    private static Set<String> levelSet = new HashSet<String>();
+    private static Set<String> sceneSet = new HashSet<String>();
+    private static Set<String> objectSet = new HashSet<String>();
+    
+    private static Boolean isAuthoring = true;
+    private String currentLevel, currentScene;
+    
+    public void creatOrModify(String order){
+        String objectID = "fakeID"; // the ID obtained after sub-parsing 
+        if (objectSet.contains(objectID)){
+            create(order);
+        }
+        else 
+            modify(order);
+    }
+    
+    public void create(String order){
+        // place where we invoke reflection to create class object Justi wrote
+    }
+    
+    public void modify(String order){
+        // place where we invoke reflection to create class object Justi wrote
+    }
+    
 }
--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,5 +1,20 @@
 package engine;
+import jgame.platform.JGEngine;
+import datamanager.*;
 
-public class Engine {
+public class Engine extends JGEngine{
+    DataManager dm = new DataManager();
 
+    @Override
+    public void initCanvas () {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void initGame () {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
