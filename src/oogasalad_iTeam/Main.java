9db38905b372b57200c33f73e399411e16e49742
anthony <anthony.olawo@duke.edu>
2014-04-06 12:20:02 -0400

    merge conflicts resolution

--- src/oogasalad_iTeam/Main.java
+++ src/oogasalad_iTeam/Main.java
@@ -1,10 +1,12 @@
 package oogasalad_iTeam;
 
+import game_authoring_environment.GAE;
+
 
 public class Main
 {
     public static void main (String[] args)
     {
-        
+     GAE g = new GAE();    
     }
 }
