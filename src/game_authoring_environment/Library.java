47aa508b3874ecf916dccd3e8079f588ebe5e5c5
Pengyi Pan <nickpanmail@gmail.com>
2014-04-06 00:34:44 -0400

    layout starter code updated

--- src/game_authoring_environment/Library.java
+++ src/game_authoring_environment/Library.java
@@ -1,10 +1,19 @@
 package game_authoring_environment;
 
+import java.awt.Dimension;
+
 import javax.swing.JTabbedPane;
 
 public class Library extends JTabbedPane {
+	
+	private static final int FULL_VIEW_HEIGHT = 768;
+	private static final int FULL_VIEW_WIDTH = 1024;
+	private static final int LIBRARY_PANEL_HEIGHT = FULL_VIEW_HEIGHT * 1/2;
+	private static final int LIBRARY_PANEL_WIDTH = FULL_VIEW_WIDTH * 1/4;
 
 	public Library(){
+		this.setPreferredSize(new Dimension(LIBRARY_PANEL_WIDTH, LIBRARY_PANEL_HEIGHT));
+		
 		
 	}
 }
