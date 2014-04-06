ceb3b1d7bc2e47909197e7acef4d995833efaa99
Pengyi Pan <nickpanmail@gmail.com>
2014-04-06 06:06:41 -0400

    minor fix

--- src/game_authoring_environment/ScenePanel.java
+++ src/game_authoring_environment/ScenePanel.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.LinkedList;
-
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComponent;
