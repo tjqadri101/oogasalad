545e0d4ffc5bb764ca3b5068ec7c4c4566579721
Justin (Zihao) Zhang <Main@wl-10-190-32-161.wireless.duke.edu>
2014-04-05 23:00:01 -0400

    create DataController

---
+++ src/controller/DataController.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class DataController {
+	//Exporter myExporter;
+	//EngineParser myEngineParser;
+	//GameFactory myFactory;
+	//Importer myImporter;
+	
+	public DataController(){
+		
+	}
+	
+	public void receiveOrder(String order){
+		
+	}
+	
+	public void receiveXML(String url){
+		
+	}
+
+}
