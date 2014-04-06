749726f643578373434071e9cf1bfccb02a03cf4
Steve Wang <sw211@duke.edu>
2014-04-05 23:29:41 -0400

    gameFactory added

---
+++ src/gameFactory/GameFactory.java
@@ -0,0 +1,24 @@
+package gameFactory;
+
+import jgame.JGObject;
+
+public class GameFactory {
+    
+    /**
+     * Only takes String ordre as argument, for creation.
+     */
+    public void processOrder(String order){
+        
+        
+    }
+    
+    /**
+     * Takes Object and String order as argument, for modification.
+     */
+    public void processOrder(JGObject object, String order){
+        
+    }
+    
+    
+
+}
