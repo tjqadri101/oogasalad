b683c3daf27bf0419f8b51cf9ed26a049638ecbc
KatKrieger <kat.krieger@gmail.com>
2014-04-06 23:10:25 -0400

    Constants

---
+++ src/oogasalad_iTeam/Constants.java
@@ -0,0 +1,12 @@
+package oogasalad_iTeam;
+
+public final class Constants {
+	public static final String CREATEACTOR = "CreateActor";
+	public static final String MODIFYACTOR = "ModifyActor";
+	public static final String BACKGROUND = "Background";
+	public static final String CREATELEVEL = "CreateLevel";
+	public static final String SWITCHSCENE = "SwitchScene";
+	public static final String MODIFYSCENE = "ModifyScene";
+	public static final String MODIFYGOALMANAGER = "ModifyGoalManager";
+	public static final String MODIFYINPUTMANAGER = "ModifyInputManager";
+}
