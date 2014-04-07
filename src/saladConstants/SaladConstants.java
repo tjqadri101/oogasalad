6acd72488fffee0c9b1661a01562d755d8561ddd
dchouzer <david.p.chou@gmail.com>
2014-04-07 02:33:00 -0400

    changed attributes to fit DataManager

--- src/saladConstants/SaladConstants.java
+++ src/saladConstants/SaladConstants.java
@@ -6,6 +6,7 @@ public final class SaladConstants {
 	public static final String BACKGROUND = "Background";
 	public static final String CREATE_LEVEL = "CreateLevel";
 	public static final String SWITCH_SCENE = "SwitchScene";
+	public static final String CREATE_SCENE = "CreateScene";
 	public static final String MODIFY_SCENE = "ModifyScene";
 	public static final String MODIFY_GOALMANAGER = "ModifyGoalManager";
 	public static final String MODIFY_INPUTMANAGER = "ModifyInputManager";
