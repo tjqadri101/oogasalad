045f1cf92b510abb19427762ba940df76f79588a
Justin (Zihao) Zhang <Main@wl-10-190-50-99.wireless.duke.edu>
2014-04-07 00:42:55 -0400

    update

--- src/controller/DataController.java
+++ src/controller/DataController.java
@@ -22,15 +22,12 @@ public class DataController {
 	
 	public DataController(){
 		myParser = new ParseGame();
-		myFactory = new GameFactory();
 		myGame = new Game();
+		myGameEngine = new GameEngine();
+		myFactory = new GameFactory(myGameEngine);
 		currentLevelID = 0;
 		currentSceneID = 0;
 		myCreateModifyTeller = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_CREATEORMODIFY);
-	}
-	
-	public void initGameEngine(){
-		myGameEngine = new GameEngine();
 	}
 	
 	/*
