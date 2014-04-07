2227312b8cf3005b3420795858476ea50e9e2171
unknown <tq4@duke.edu>
2014-04-07 15:45:06 -0400

    Commented out data controller

--- src/controller/GAEController.java
+++ src/controller/GAEController.java
@@ -6,25 +6,25 @@ public class GAEController {
 	private DataController myDataController;
 	
 	public GAEController(){
-		myDataController = new DataController();
+		//myDataController = new DataController();
 	}
 	
 	public void createPlayer(int ID,String url,String name){
 		String order = SaladConstants.CREATE_PLAYER + ",ID,"+ID+",Image,"+url+",Position,0,0,Name,"+name;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
  	
 	public void createActor(int ID,String url,String name){
 		String order = SaladConstants.CREATE_ACTOR + ",ID,"+ID+",Image,"+url+",Position,0,0,Name,"+name;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 		
 	}
 	
 	public void deleteActor(int ID){
 		String order = "DeleteActor,ID,"+ID;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 		
 	}
@@ -32,50 +32,50 @@ public class GAEController {
 	
 	public void modifyActorPosition(int ID, double xPos, double yPos){
 		String order = SaladConstants.MODIFY_ACTOR + ",ID,"+ID+",Position," + xPos + "," + yPos;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
 	public void modifyActorSpeed(int ID, double xSpeed, double ySpeed){
 		String order = SaladConstants.MODIFY_ACTOR + ",ID,"+ID+",Position," + xSpeed + "," + ySpeed;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	public void createLevel(int ID){
 		String order = SaladConstants.CREATE_LEVEL + ",ID,"+ID;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
 	public void modifyLevel(int ID, String function){
 		String order = SaladConstants.MODIFY_LEVEL + ",ID,"+ID+ function;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
 	public void switchLevel(int ID){
 		String order = SaladConstants.SWITCH_LEVEL + ",ID,"+ID;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
 	
 	public void createScene(int ID, String path){
 		String order = SaladConstants.CREATE_SCENE + ",ID,"+ID+",Image,"+path;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
 	public void modifyScene(int ID, String function){
 		String order = SaladConstants.MODIFY_SCENE + ",ID,"+ID+ function;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 		
 	}
 	
 	public void switchScene(int ID){
 		String order = SaladConstants.SWITCH_SCENE + ",ID,"+ID;
-		myDataController.receiveOrder(order);
+		//myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
 	
