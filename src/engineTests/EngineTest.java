4489a46c357bf43574f673de985e90f3703f31bc
Shenghan Chen <sc317@duke.edu>
2014-04-10 23:35:56 -0400

    game engine update

--- src/engineTests/EngineTest.java
+++ src/engineTests/EngineTest.java
@@ -5,20 +5,26 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jbox2d.dynamics.Body;
+
+import stage.Game;
+
 import engine.GameEngine;
 
 public class EngineTest {
 	public static void main(String[] arg){
 		JFrame mainFrame = new JFrame("test");
 		GameEngine ge = new GameEngine();
+		Game g = new Game();
+		ge.setGame(g);
 		JPanel jp = new JPanel();
 		jp.add(ge);
 		mainFrame.add(jp, BorderLayout.CENTER);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
-		
 		System.out.print("lol\n");
 		ge.createPlayer(0, "actor_default.png", 0, 0, null, 0);
-		ge.createBackground("actor_default.png");
+		g.getPlayer().setSpeed(1, 2);
+		//ge.createBackground("actor_default.png");
     }
 }
