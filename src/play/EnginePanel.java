3305b623f6b6873c0b4338ca8e03d28d719e810e
dchouzer <david.p.chou@gmail.com>
2014-04-16 22:58:07 -0400

    Changed panel to fit datacontroller method

--- src/play/EnginePanel.java
+++ src/play/EnginePanel.java
@@ -27,12 +27,14 @@ public class EnginePanel extends JPanel {
 	private GameEngine gameEngine;
 	private Game game;
 	
+	private static final boolean IS_EDITING = false;
+	
 	public EnginePanel(DataController dc) {
 		this.dc = dc;
 		game = new Game();
 		setLayout(new BorderLayout());
 		setPreferredSize(new Dimension(600,800));
-		gameEngine = dc.initGameEngine();
+		gameEngine = dc.initGameEngine(IS_EDITING);
 		add(gameEngine, BorderLayout.CENTER);
 		setVisible(true);
 	}
