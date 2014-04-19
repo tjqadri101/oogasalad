87f43d30eee6972ddd927b99265a6c613b7ec5ad
dchouzer <david.p.chou@gmail.com>
2014-04-19 14:13:54 -0400

    Fully implemented imageBuffer - ready for use in GAE

--- src/imagebuffer/ImageBufferTest.java
+++ src/imagebuffer/ImageBufferTest.java
@@ -1,14 +1,33 @@
 package imagebuffer;
 
-import static org.junit.Assert.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.IOException;
 
-import org.junit.Test;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ImageBufferTest {
+@SuppressWarnings("serial")
+public class ImageBufferTest extends JFrame {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private ImageBuffer myImageBuffer;
+	private JPanel myPanel;
+	
+	public ImageBufferTest() {
+		myPanel = createPanel();
+		myImageBuffer = new ImageBuffer(myPanel);
 	}
-
+	
+	public JPanel createPanel() {
+		JPanel panel = new JPanel();
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(600,800));
+		setVisible(true);
+		return panel;
+	}
+	
+	public static void main (String args[]) throws IOException {
+		ImageBufferTest ibt = new ImageBufferTest();
+		ibt.myImageBuffer.loadAndSave();
+	}
 }
