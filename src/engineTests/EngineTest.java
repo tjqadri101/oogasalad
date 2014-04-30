7e237b1cd77106d009cb061479db831fa2b535dd
Shenghan Chen <sc317@duke.edu>
2014-04-30 19:44:07 -0400

    update

--- src/engineTests/EngineTest.java
+++ src/engineTests/EngineTest.java
@@ -115,12 +115,9 @@ public class EngineTest {
 		game.getScoreManager().setValue(50, "LevelDone", 1);
 		game.getGravity().setMagnitude(0.1);
 		game.getLiveManager().setInitLives(3, 0);
-		game.getInputManager().setKey('G', "LifeIncrease");
-		game.getInputManager().setKey('F', "GameOver");
-		
+		game.getInputManager().setKey('I', "LifeIncrease");
 		game.getInputManager().setKey('O', "GameOver");
-		game.getInputManager().setKey('K', "LifeLost");
-		game.getInputManager().setKey('L', "LevelDone");
+		game.getInputManager().setKey('D', "LevelDone");
 		
 		engine.gotoGameState("Title");
 		engine.setCurrentScene(1, 0);
