ba620962e9572be632ed1fb77cbfbb8fe50690be
unknown <tq4@duke.edu>
2014-04-30 17:23:51 -0400

    Typos and commented out code removed in RightPanel

--- src/game_authoring_environment/RightPanel.java
+++ src/game_authoring_environment/RightPanel.java
@@ -3,55 +3,30 @@
  */
 package game_authoring_environment;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
+import java.awt.Container;
+
 import javax.swing.JComponent;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JSplitPane;
-import javax.swing.JTextField;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.plaf.LayerUI;
-
 import reflection.ReflectionException;
-
 import java.lang.reflect.Field;
-
-import jgame.platform.JGEngine;
-
 import controller.GAEController;
-import engine.GameEngine;
-import engineTests.EngineTest;
 
+
+@SuppressWarnings("serial")
 public class RightPanel extends JSplitPane {
 
 	public double curXPos, curYPos;
 	public GAEController myGAEController;
+	//public JSpinner xSpinner, ySpinner;
 	public static final double POSIT_STEP = 10d;
 	
 	public RightPanel(GAEController gController){
