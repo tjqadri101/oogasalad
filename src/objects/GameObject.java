1132fe5d21f28d353dead01a5355870641affe3d
Justin (Zihao) Zhang <Main@wl-10-190-44-121.wireless.duke.edu>
2014-04-30 02:41:47 -0400

    update

--- src/objects/GameObject.java
+++ src/objects/GameObject.java
@@ -492,7 +492,7 @@ public abstract class GameObject extends JGObject {
 	 */
 	public List<String> getAttributes() {
 		myAttributes.addAll(myActionManager.getAttributes());
-//		myAttributes.addAll(myAnimationManager.getAttributes());
+		myAttributes.addAll(myAnimationManager.getAttributes());
 		return myAttributes;
 	}
 
