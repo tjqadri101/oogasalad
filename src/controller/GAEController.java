1a65747e33ce22e9f61ad01e29c12d0b79ebab01
unknown <tq4@duke.edu>
2014-05-01 03:40:53 -0400

    Corrected GAEController typos

--- src/controller/GAEController.java
+++ src/controller/GAEController.java
@@ -313,6 +313,39 @@ public class GAEController {
 		if (!DEBUG) myDataController.receiveOrder(order);
 		System.out.println(order);
 	}
+	/**
+	 * Animate an player's jump animation
+	 */
+	public void modifyPlayerAnimationJump(String url, int xSize, int ySize ){
+		String order = SaladConstants.MODIFY_ACTOR_ANIMATION + SaladConstants.SEPARATOR + SaladConstants.ID + 
+				SaladConstants.SEPARATOR + playerID + SaladConstants.SEPARATOR + SaladConstants.JUMP_ANIMATION +
+				SaladConstants.SEPARATOR +  SaladConstants.JUMP + SaladConstants.SEPARATOR + url + SaladConstants.SEPARATOR + 
+				xSize + SaladConstants.SEPARATOR + ySize; 
+		if (!DEBUG) myDataController.receiveOrder(order);
+		System.out.println(order);
+	}
+
+	
+	/**
+	 *  Animate an player's forward move animation
+	 */
+	public void modifyPlayerAnimationFDMove(String imgURL, int xSize, int ySize){ 
+		String order = SaladConstants.MODIFY_ACTOR_ANIMATION + SaladConstants.SEPARATOR + SaladConstants.ID + SaladConstants.SEPARATOR + playerID
+				+ SaladConstants.SEPARATOR + SaladConstants.FD_MOVE + SaladConstants.SEPARATOR + SaladConstants.FD_MOVE
+				+ SaladConstants.SEPARATOR + imgURL + SaladConstants.SEPARATOR + xSize + SaladConstants.SEPARATOR + ySize;
+		if (!DEBUG) myDataController.receiveOrder(order);
+		System.out.println(order);
+	}
+	
+	/**
+	 * Animate an player's backward move animation
+	 */
+	public void modifyPlayerAnimationBKMove(String imgURL, int xSize, int ySize){ 
+		String order = SaladConstants.MODIFY_ACTOR + SaladConstants.SEPARATOR + SaladConstants.ID + SaladConstants.SEPARATOR + playerID + SaladConstants.SEPARATOR + SaladConstants.BK_MOVE + SaladConstants.SEPARATOR + 
+				SaladConstants.BK_MOVE + SaladConstants.SEPARATOR + imgURL + SaladConstants.SEPARATOR + xSize + SaladConstants.SEPARATOR + ySize ;
+		if (!DEBUG) myDataController.receiveOrder(order);
+		System.out.println(order);
+	}
 
 	/**
 	 * Delete player without providing player id. The already specified playerID is used for the id.
@@ -352,7 +385,8 @@ public class GAEController {
 	 * Modify actor's jump 
 	 */
 	public void modifyActorJump(double magnitude, int numberOfJumpsAllowedInAir){
-		String order = SaladConstants.MODIFY_ACTOR + SaladConstants.SEPARATOR + selectedActorID + SaladConstants.SEPARATOR + SaladConstants.JUMP + SaladConstants.SEPARATOR +
+		String order = SaladConstants.MODIFY_ACTOR + SaladConstants.SEPARATOR + SaladConstants.ID +  SaladConstants.SEPARATOR +
+				selectedActorID + SaladConstants.SEPARATOR + SaladConstants.JUMP + SaladConstants.SEPARATOR +
 				SaladConstants.JUMP + SaladConstants.SEPARATOR + magnitude + SaladConstants.SEPARATOR + numberOfJumpsAllowedInAir;
 		if (!DEBUG) myDataController.receiveOrder(order);
 		System.out.println(order);
@@ -408,7 +442,7 @@ public class GAEController {
 	 * Animate an actor's backward move animation
 	 */
 	public void modifyActorAnimationBKMove(String imgURL, int xSize, int ySize){ 
-		String order = SaladConstants.MODIFY_ACTOR + SaladConstants.SEPARATOR + SaladConstants.ID + SaladConstants.SEPARATOR + playerID + SaladConstants.SEPARATOR + SaladConstants.BK_MOVE + SaladConstants.SEPARATOR + 
+		String order = SaladConstants.MODIFY_ACTOR + SaladConstants.SEPARATOR + SaladConstants.ID + SaladConstants.SEPARATOR + selectedActorID + SaladConstants.SEPARATOR + SaladConstants.BK_MOVE + SaladConstants.SEPARATOR + 
 				SaladConstants.BK_MOVE + SaladConstants.SEPARATOR + imgURL + SaladConstants.SEPARATOR + xSize + SaladConstants.SEPARATOR + ySize ;
 		if (!DEBUG) myDataController.receiveOrder(order);
 		System.out.println(order);
