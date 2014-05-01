12f26d12890824aeadf6e8bd23f8e07eaef93835
Steve Wang <sw211@duke.edu>
2014-05-01 01:40:38 -0400

    merge conflict

--- src/engineManagers/SoundManager.java
+++ src/engineManagers/SoundManager.java
@@ -9,9 +9,16 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.SourceDataLine;
 import objects.GameObject;
+import util.GeneralInputBox;
 import util.Music;
 
 public class SoundManager extends Music {
+    private static final String SRC_ENGINE_SOUNDS_CLIP_SUCCEED_WAV = "src/engine/Sounds/ClipSucceed.wav";
+    private static final String SRC_ENGINE_SOUNDS_GAME_OVER_WAV = "src/engine/Sounds/GameOver.wav";
+    private static final String SRC_ENGINE_SOUNDS_CLIP_OHYEAH_WAV = "src/engine/Sounds/ClipOhyeah.wav";
+    private static final String SUCCEED = "Succeed";
+    private static final String GAMEOVER = "Gameover";
+    private static final String OHYEAH = "Ohyeah";
     private static final String SRC_ENGINE_SOUNDS_PLAY_JOYFUL_WAV = "src/engine/Sounds/PlayJoyful.wav";
     private static final String SRC_ENGINE_SOUNDS_PLAY_CREEPY_WAV = "src/engine/Sounds/PlayCreepy.wav";
     private static final String HAPPY = "Happy";
@@ -27,24 +34,35 @@ public class SoundManager extends Music {
     
     private int BUFFER_SIZE = 12800000;
     
+    public SoundManager(){
+        mySoundMap = new HashMap<String,String>();
+        mySoundMap.put(CREEPY, SRC_ENGINE_SOUNDS_PLAY_CREEPY_WAV);
+        mySoundMap.put(HAPPY, SRC_ENGINE_SOUNDS_PLAY_JOYFUL_WAV);
+        mySoundMap.put(OHYEAH, SRC_ENGINE_SOUNDS_CLIP_OHYEAH_WAV);
+        mySoundMap.put(GAMEOVER, SRC_ENGINE_SOUNDS_GAME_OVER_WAV);
+        mySoundMap.put(SUCCEED, SRC_ENGINE_SOUNDS_CLIP_SUCCEED_WAV);
+        chooseInitSound();
+    }
+    
     /**
-     * 
      * @param filename String of the file path to the music file
      */
     public SoundManager(String filename) {
             super(filename);
-            mySoundMap = new HashMap<String,String>();
-            mySoundMap.put(CREEPY, SRC_ENGINE_SOUNDS_PLAY_CREEPY_WAV);
-            mySoundMap.put(HAPPY, SRC_ENGINE_SOUNDS_PLAY_JOYFUL_WAV);
     }
-    
+
+    /**
+     * @param ask user for what music to put in
+     */
     public void chooseInitSound(){
         System.out.print("Pick the game mode: Creepy or Happy??? ");
         String input = System.console().readLine();
-        myBackgroud = mySoundMap.get(input);
+        myBackgroud = mySoundMap.get(GeneralInputBox.showMessageBox());
     }
     
-
+    public void addSoundClip(String key, String fileName){
+        mySoundMap.put(key, fileName);
+    }
 
     public List<String> getAttributes() {
         return null;
