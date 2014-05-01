c48ecc052d7b3ab786a9663810c87ce43ca51ecf
Justin (Zihao) Zhang <Main@wl-10-190-32-50.wireless.duke.edu>
2014-05-01 03:25:13 -0400

    update

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -726,8 +726,11 @@ public class GameEngine extends StdGame {
 		 object.setSize(xsize, ysize);
 	 }
 
-	 public void setObjectImage(GameObject object, String action, String imgfile, int xsize, int ysize){
+	 public void setObjectImage(int objectID, String action, String imgfile, int xsize, int ysize){
 		 loadImage(imgfile);
+		 GameObject object;
+		 if(objectID != 0) object = myGame.getNonPlayer(myCurrentLevelID, myCurrentSceneID, objectID);
+		 else object = myGame.getPlayer(objectID);
 		 object.setSize(object.getXSize(), object.getYSize());
 		 object.modifyDynamicImage(action, imgfile, xsize, ysize);
 	 }
